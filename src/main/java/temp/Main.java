6a51d06dc6a21547012b1aa3b59f88801ba46f66
Amit Verma <amitv@contatan.local>
2017-02-21 17:15:20 +0530

    Initial Commit

---
+++ src/main/java/temp/Main.java
@@ -0,0 +1,20 @@
+package temp;
+
+public class Main {
+	
+	/**  
+	   * this is my public method example  
+	   */  
+	  public void FFoo(int arg1, int arg2, int arg3) {  
+	   String At="";
+	   if (false)  
+	   {  
+	     System.out.println("if");  
+	   }  
+	   else  
+	   {  
+	     System.out.println("else");  
+	   }  
+	  }
+
+}
---
+++ src/main/java/temp/Main1.java
@@ -0,0 +1,15 @@
+package temp;
+/**
+ * This is exaple class for checkstyle
+ * @author amitv
+ *
+ */
+public class Main1 {
+  
+  /**
+  * This is example method to support standards
+  */
+  public void display ( ) {
+    System.out.println( "display something" );
+  }
+}
